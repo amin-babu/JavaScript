dc21a4157395d0e5d6297d52370269f17446f9d1
aminbabu12 <badrulaminbabu@gmail.com>
2025-08-08 15:07:34 +0600

    notes updated

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-function Person(first, last, age){
-  this.firstName = first;
-  this.lastName = last;
-  this.age = age;
-  this.fullName = function(){
-    return this.firstName + ' ' + this.lastName;
-  };
-};
-
-const rakib = new Person('Rakib', 'Hossain', 22);
-let sakib = new Person('Sakib', 'Al Hasan', 32);
-
-sakib.country = 'Bangladesh';
-
-console.log(rakib);
+function addition(a, b){
+  console.log(a + b)
+}
+addition(7, 6);
