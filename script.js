b5e6a77d2b00207a8749793fbf4f279a39ae8a68
aminbabu12 <badrulaminbabu@gmail.com>
2025-08-16 00:39:19 +0600

    mertering at array

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango", 'Tango', 'lango', 78, false];
-console.log(fruits.toString());
-console.log(fruits)
+const numbers = [40, 5, 12, 5, 17, 50, 30];
+
+function myFunction(value, index, array){
+  return value > 60;
+}
+
+const newNumbers = numbers.some(myFunction);
+console.log(numbers.includes(5, 4));
