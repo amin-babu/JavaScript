40fcb6267204290cbf20bb856392ff1c27a0f138
aminbabu12 <badrulaminbabu@gmail.com>
2026-04-08 11:13:52 +0600

    DOM done

--- new.js
+++
@@ -1,3 +0,0 @@
-export const sleep = () => console.log('I will sleep');
-export let a = 'Amin';
-export let age = 22;
--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-import * as test from './new.js';
-console.log(test.sleep());
+const input = document.querySelector('input[type="text"]');
+input.addEventListener('input', function (e) {
+  console.log(e.key);
+});
